ead3f93b9844e8e74d358d0bd9334647b045c37e
Sean McCormack <73062492+SeanMcCormack23@users.noreply.github.com>
2022-01-17 21:31:02 +0000

    Add files via upload

--- cart/script.js
+++ cart/script.js
@@ -1,32 +1,15 @@
-
-
-// added.addEventListener("click", aFunt);
-// function aFunt() {
-//   console.log("clicked");
-// }
-
-// added.addEventListener("click", event => {
-//   console.log("yes");
-// \ \ })
-// var cartList = document.getElementById("list");
-
-// function doAll(){
-//   var key = "";
-//   var list = "<tr><th>Item</th><th>Value</th><tr\n>";
-//   var i = 0;
-  
-//   for (i = 0; i < localStorage.length-1; i++) {
-//   key = localStorage.key(i);
-//   list += "<tr><td>" + key + "</td>\n<td>" + localStorage.getItem(key) + "</td></tr>\ n";
-
-//   cartList.innerHTML = list;
-//   }
-// }
 var added = document.getElementById("addToCart");
 var cart = document.getElementById("cartText");
-var amount = [];
+//click event listener to add product to cart
+//using local storage (browser)
+//
+var x = 1;
 added.addEventListener("click", event => {
-  amount.push(1);
-  var len = amount.length;
-  return len;
+//pushing a value onto cartItem array
+  localStorage.setItem('cartItem', x++);
+  var cartItem = localStorage.getItem("cartItem");
+  cart.innerHTML = "Your cart contains: " + cartItem[cartItem.length-1];
+  console.log("clik");
+//only display the last value in the array
 })
+
